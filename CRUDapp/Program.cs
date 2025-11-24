55dc9f883083a03a35796a988afabf64bf381d13
ikmori <elliamperez2002@gmail.com>
2025-11-24 00:09:47 -0400

    Add project files.

---
+++ CRUDapp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SimpleCrudApp
+{
+
+    public class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.Size = new Size(600, 500);
+            this.Text = "Gestor de tareas ";
+
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
